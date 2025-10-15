3c772747490c53787e1e65214b41450be03555cc
Francisco Pagnotta <franciscopagnotta@gmail.com>
2025-10-15 15:16:53 -0300

    login sin schema

--- backend/loginBack.js
+++ backend/loginBack.js
@@ -12,8 +12,8 @@ module.exports = async (req, res) => {
             .input("email", sql.VarChar, email)
             .query(`
                 SELECT u.ID_Usuario, w.Password
-                FROM a002103.USUARIO u
-                inner join  a002103.WEB_PORTAL_ITAS_USR w ON u.ID_Usuario = w.ID_Usuario
+                FROM USUARIO u
+                JOIN WEB_PORTAL_ITAS_USR w ON u.ID_Usuario = w.ID_Usuario
                 WHERE u.Email = @email
             `);
 
