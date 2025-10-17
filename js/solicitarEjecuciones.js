a087a1733eba1a3371652bbed02f0c53225aa59d
Francisco Pagnotta <franciscopagnotta@gmail.com>
2025-10-17 15:32:45 -0300

    P2_SolicitarEjecuiones

--- js/solicitarEjecuciones.js
+++ js/solicitarEjecuciones.js
@@ -2,8 +2,7 @@
 const dropdown = document.getElementById("dropdownFlujos");
 const detalle = document.getElementById("detalleFlujo");
 const instrucciones = document.getElementById("instruccionesFlujo");
-const flujoTipoSpan = document.getElementById("flujoTipo");
-const prioSpan = document.getElementById("prioFlujo");
+
 const datosSolicitados = document.getElementById("datosSolicitados");
 const inputUsuario = document.getElementById("inputUsuario");
 
@@ -31,14 +30,12 @@ dropdown.addEventListener("change", () => {
         detalle.value = flujoSeleccionado.detalle;
         instrucciones.value = flujoSeleccionado.instrucciones;
         datosSolicitados.textContent = flujoSeleccionado.campos;
-        flujoTipoSpan.textContent = flujoSeleccionado.flujoTipo;
-        prioSpan.textContent = flujoSeleccionado.prio;
+
     } else {
         detalle.value = "";
         instrucciones.value = "";
         datosSolicitados.textContent = "";
-        flujoTipoSpan.textContent = "";
-        prioSpan.textContent = "";
+
     }
 });
 
@@ -51,23 +48,20 @@ btnLimpiar.addEventListener("click", () => {
     instrucciones.value = "";
     datosSolicitados.textContent = "";
     inputUsuario.value = "";
-    flujoTipoSpan.textContent = "";
-    prioSpan.textContent = "";
+
 });
 
 btnEnviar.addEventListener("click", () => {
     const flujo = dropdown.value;
     const datosValor = datosSolicitados.textContent;
-    const tipoFlujo = flujoTipoSpan.textContent;
-    const prioFlujoValor = prioSpan.textContent;
+
     const solicitante = localStorage.getItem("usuario") || "UsuarioDemo";
     const identificador = inputUsuario.value;
 
     const payload = {
         flujoSeleccionado: flujo,
         datos: datosValor,
-        tipoFlujo,
-        prioridad: prioFlujoValor,
+
         solicitante,
         identificador,
         estado: "En proceso",
