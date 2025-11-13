1ed865701ae5ed685b78ed4fd62bc9dc508ab921
Gabi2172 <gmallorquin@teco.com.ar>
2025-11-13 15:43:27 -0300

    limpiar_prioridad

--- js/AppOrdenesSf_GaleriaScript.js
+++ js/AppOrdenesSf_GaleriaScript.js
@@ -11,7 +11,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 bajada: celdas[0].textContent.trim(),
                 negocio: celdas[1].textContent.trim(),
                 descripcion: celdas[2].textContent.trim(),
-                prioridad: celdas[3].textContent.trim(),
                 esquema: celdas[4].textContent.trim(),
                 activar: celdas[5].textContent.trim()
             };
@@ -21,7 +20,6 @@ document.addEventListener("DOMContentLoaded", () => {
             document.getElementById("selectNegocio").value = datos.negocio;
             document.getElementById("textareaScript").value = datos.descripcion; // Aquí luego irá el script real
             document.getElementById("selectEsquema").value = datos.esquema;
-            document.getElementById("selectPrioridad").value = datos.prioridad;
             document.getElementById("selectActivar").value = datos.activar;
 
             modal.show();
