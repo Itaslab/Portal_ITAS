20c873c90a1f0898992cbdbedf643e4db747a2f2
Gabi2172 <gmallorquin@teco.com.ar>
2025-11-20 10:52:01 -0300

    GaleriaUsr_004_P

--- js/appordenesGaleriausuarios.js
+++ js/appordenesGaleriausuarios.js
@@ -318,24 +318,7 @@ if (estadoSpan) {
       resultado.innerHTML = `<div class="alert alert-warning">Modo SCRIPT requiere que ingrese el script.</div>`;
       return;
     }
-	 const textareaScript = document.getElementById('modalScript');
-
-  // Habilitar/deshabilitar textarea según Modo
-  function updateScriptState() {
-    const modoVal = modoSelect.value;
-    if (modoVal === 'SCRIPT') {
-      textareaScript.disabled = false;
-      textareaScript.classList.remove('bg-light');
-      textareaScript.required = true;
-    } else {
-      textareaScript.disabled = true;
-      textareaScript.value = '';
-      textareaScript.classList.add('bg-light');
-      textareaScript.required = false;
-    }
-  }
-	  
-
+	
     try {
       const resp = await fetch("/usuarios/actualizar", {
         method: "POST",
