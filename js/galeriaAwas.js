09d384fc617c6636b398547d84d1614c3933d18b
Francisco Pagnotta <franciscopagnotta@gmail.com>
2026-06-11 14:48:58 -0300

    renderTabla

--- js/galeriaAwas.js
+++ js/galeriaAwas.js
@@ -190,15 +190,17 @@ async function cargarAWAS() {
       row.innerHTML = `
   <td>${awa.ID_WA ?? "-"}</td>
   <td>${awa.ID_AWA ?? "-"}</td>
-  <td style="width:50%;">
-    <span class="titulo-awa">${awa.Titulo}</span>
-    <span class="estrellas-awa">${estrellas}</span>
+<td>
+    <div class="titulo-cell">
+      <span class="titulo-awa">${awa.Titulo}</span>
+      <span class="estrellas-awa">${estrellas}</span>
+    </div>
   </td>
   <td>${awa.Origen ?? "-"}</td>
   <td class="${estadoColor} fw-bold">${awa.Estado ?? "-"}</td>
 
-  <td>
-    <div class="d-flex justify-content-end flex-wrap gap-2 acciones-awa">
+  <td class="text-end">
+    <div class="d-flex justify-content-end gap-2 acciones-awa">
 
       <button 
         class="btn ${btnClass} btn-sm text-white"
