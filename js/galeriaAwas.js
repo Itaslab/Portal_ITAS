a3b797a8afc6b19f78e3fd0ab20fc0433d600d74
Francisco Pagnotta <franciscopagnotta@gmail.com>
2026-06-26 16:09:39 -0300

    css

--- js/galeriaAwas.js
+++ js/galeriaAwas.js
@@ -265,7 +265,7 @@ async function cargarAWAS() {
   <td>${awa.Origen ?? "-"}</td>
   <td class="${estadoColor} fw-bold">${awa.Estado ?? "-"}</td>
 
-  <td class="text-end"  style="width: 180px;">
+  <td class="text-end">
     <div class="d-flex justify-content-end gap-2 acciones-awa">
 
 <button 
